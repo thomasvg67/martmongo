eb22e0e315cd134be8f2269703405548a4f81ff8
thomasvg67 <thomasvgvpkl123@gmail.com>
2025-05-20 12:32:42 +0530

    change in import in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Home from './pages/home'
+import Home from './pages/Home'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import About from './pages/About'
 import Contact from './pages/Contact'
